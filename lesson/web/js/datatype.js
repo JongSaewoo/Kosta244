24c5b492b2d5f29bfd4eb9c519ac45274cce390f
JongWoo <sitsumonyou@nate.com>
2022-06-08 16:53:23 +0900

    weblesson file upload

---
+++ lesson/web/js/datatype.js
@@ -0,0 +1,12 @@
+var a;
+console.log(typeof(a));//undefined
+a = 123;
+console.log(typeof(a));//number
+a = 'hello';
+console.log(typeof(a));//string
+a = true;
+console.log(typeof(a));//boolean
+a = function(){};
+console.log(typeof(a));//function
+a = {};
+console.log(typeof(a));//object
